900d16ab6bb763bb7b0919ecb1fd1a4f22e5a6be
rikaaa0928 <8528731+rikaaa0928@users.noreply.github.com>
2025-10-17 10:58:01 +0800

    upgrade tonic prost

--- leaf/build.rs
+++ leaf/build.rs
@@ -127,7 +127,9 @@ fn main() {
     }
 
     // ROG protobuf generation
-    if std::path::Path::new("src/proxy/rog/proto/rog.proto").exists() {
-        tonic_build::compile_protos("src/proxy/rog/proto/rog.proto");
+    if std::env::var("CARGO_FEATURE_OUTBOUND_ROG").is_ok() {
+        if std::path::Path::new("src/proxy/rog/proto/rog.proto").exists() {
+            tonic_prost_build::compile_protos("src/proxy/rog/proto/rog.proto");
+        }
     }
 }
